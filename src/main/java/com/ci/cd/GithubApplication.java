077c1ff859c7ab778ac2455529ebd09c77f8aa6a
Kisna_Ks <kskrish75@gmail.com>
2020-10-02 17:24:51 +0530

    Initial commit

---
+++ src/main/java/com/ci/cd/GithubApplication.java
@@ -0,0 +1,13 @@
+package com.ci.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApplication.class, args);
+	}
+
+}
